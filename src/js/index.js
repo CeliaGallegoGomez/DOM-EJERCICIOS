d8496ad02a936b9e0433c9dede54211dc97184f6
CeliaGallegoGomez <cela17@hotmail.es>
2024-06-21 18:52:35 +0200

    update

--- src/js/index.js
+++ src/js/index.js
@@ -1,2 +1,13 @@
-// El styles lo importamos aquí, ya se carga después al compilar todo
-import '../scss/styles.scss';
+import "../scss/styles.scss";
+
+// - Partiendo de este HTML selecciona en JavaScript el h3 y utilizándolo como punto de partida imprime la información de todos los elementos que hay en formato "familia": Soy un h3 con el id... y la clase... mi padre es un header con.... mi hermano es un h1 con....
+
+const titleElement = document.getElementById("start-point");
+
+
+
+console.log(
+  `Soy un ${titleElement.tagName} con el id ${titleElement.id} y ${titleElement.parentElement.className}. Mi padre es un header con id ${} y clase ${}.Mi hermano ${} con id ${} y clase ${}
+  "
+`
+);
